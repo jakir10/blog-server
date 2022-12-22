d6b26203778600c4938b79dd239985ac38e0684c
jakir10 <jakir.cse.bubt@gmail.com>
2022-12-23 00:06:06 +0600

    final

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
-require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const { MongoClient, ObjectId } = require("mongodb");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-const cors = require("cors");
-
-app.use(cors());
 app.use(express.json());
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q66zrl2.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+app.use(cors());
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2sjsg0j.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, });
 
 async function run() {
   try {
     await client.connect();
     console.log("db-connect");
 
-    const productCollection = client.db('blog_tech').collection('product');
-    const blogCollection = client.db('blog_tech').collection('blog');
+    const BlogCollection = client.db('Techblog').collection("blog");
 
     app.post("/blog", async (req, res) => {
       const data = req.body;
       console.log(data);
-      const product = await blogCollection.insertOne(data);
-      res.send(product);
+      const blog = await BlogCollection.insertOne(data);
+      res.send(blog);
     });
     app.get("/blog", async (req, res) => {
-      const result = await blogCollection.find({}).toArray();
+      const result = await BlogCollection.find({}).toArray();
       res.send(result);
     });
     app.delete("/blog/:id", async (req, res) => {
       const id = req.params.id;
       console.log(id);
-      const result = await blogCollection.deleteOne({ _id: ObjectId(id) });
+      const result = await BlogCollection.deleteOne({ _id: ObjectId(id) });
       res.send(result);
     });
     app.patch("/blog/:id", async (req, res) => {
@@ -47,12 +42,12 @@ async function run() {
       const updatedDoc = {
         $set: req.body,
       };
-      const result = await blogCollection.updateOne(filter, updatedDoc);
+      const result = await BlogCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
 
     app.get('/', (req, res) => {
-      res.send("server running")
+      res.send("Blog server running")
     })
   } finally {
   }
@@ -62,56 +57,3 @@ run().catch(console.dir);
 app.listen(port, () => {
   console.log(`app listening on ${port}`);
 });
-
-
-
-// const run = async () => {
-//   try {
-//     const db = client.db("blog_tech");
-//     const blogCollection = db.collection("blog");
-
-//     app.get("/blog", async (req, res) => {
-//       const cursor = blogCollection.find({});
-//       const blog = await cursor.toArray();
-
-//       res.send({ status: true, data: blog });
-//     });
-
-//     app.post("/blog", async (req, res) => {
-//       const data = req.body;
-
-//       const result = await blogCollection.insertOne(data);
-
-//       res.send(result);
-//     });
-
-//     app.delete("/blog/:id", async (req, res) => {
-//       const id = req.params.id;
-
-//       const result = await blogCollection.deleteOne({ _id: ObjectId(id) });
-//       res.send(result);
-//     });
-
-//     app.patch("/blog/:id", async (req, res) => {
-//       const id = req.params.id;
-//       const filter = { _id: ObjectId(id) };
-//       const updatedDoc = {
-//         $set: req.body,
-//       };
-//       const result = await BlogCollection.updateOne(filter, updatedDoc);
-//       res.send(result);
-//     });
-
-//   } finally {
-//   }
-// };
-
-// run().catch((err) => console.log(err));
-
-// app.get("/", (req, res) => {
-//   res.send("Blog Server Run");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
